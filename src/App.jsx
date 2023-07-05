bdff10102b6c3a0320e2c0a1947ed5b4342e24e3
Szwederowiak <pawel.kobylarski@op.pl>
2023-07-05 17:15:54 +0200

    1 - init app

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import './App.scss'
+
+const MOVIES = [
+  {
+    "id": 1,
+    "title": "S01E01 - Umarł odbiornik, niech żyje odbiornik!",
+    "link": "http://ipla-e1-78.pluscdn.pl/p/movies/8f/8fc6aa7d3808f476dee9b99722343b30.mp4"
+  },
+  {
+    "id": 2,
+    "title": "S01E02 - Wiara czyni cuda",
+    "link": "http://ipla-e2-21.pluscdn.pl/p/movies/b1/b1272b01cd09906e7fbbd55e44f01b84.mp4"
+  },
+  {
+    "id": 3,
+    "title": "S01E03 - Kiepscy – zboczeńcy",
+    "link": "http://ipla-e1-79.pluscdn.pl/p/movies/ed/ed98b6aef9b52a15baf1d5c0063bd2da.mp4"
+  },
+  {
+    "id": 4,
+    "title": "S01E04 - Rekord Guinnessa",
+    "link": "http://ipla-e2-21.pluscdn.pl/p/movies/ae/aee7dda8d36c759f645fa124a5a32196.mp4"
+  },
+  {
+    "id": 5,
+    "title": "S01E05 - Kiepski film",
+    "link": "http://ipla-e1-82.pluscdn.pl/p/movies/38/389ec01d7eaef0b53cd92d893ce37193.mp4"
+  },
+  {
+    "id": 6,
+    "title": "S01E06 - Umcia, Umcia",
+    "link": "http://ipla-e1-77.pluscdn.pl/p/movies/86/86f8c9635ffbd813af6904b82eca4438.mp4"
+  },
+  {
+    "id": 7,
+    "title": "S01E07 - Kiepski czarnowidz",
+    "link": "http://ipla-e2-21.pluscdn.pl/p/movies/77/7756c8d6d483415c744a9baee9febd7a.mp4"
+  },
+  {
+    "id": 8,
+    "title": "S01E08 - Kiepskie żarty",
+    "link": "http://ipla-e2-17.pluscdn.pl/p/movies/b4/b470b864e153dd750ff976e9b3f931a4.mp4"
+  },
+  {
+    "id": 9,
+    "title": "S01E09 - Kiepski mózg",
+    "link": "http://ipla-e1-82.pluscdn.pl/p/movies/2d/2d4b136479f74a5de43c1caf45a476c8.mp4"
+  },
+  {
+    "id": 10,
+    "title": "S01E10 - Kiepscy mordercy",
+    "link": "http://ipla-e1-79.pluscdn.pl/p/movies/ed/edf8f4d8ff001e96ca8450a4f9c38b36.mp4"
+  },
+  {
+    "id": 11,
+    "title": "S01E11 - Gumowy interes",
+    "link": "http://ipla-e1-77.pluscdn.pl/p/movies/37/379c6094f020fce8db67a93c52cd5ec2.mp4"
+  },
+  {
+    "id": 12,
+    "title": "S01E12 - Rolki, czyli total gigant",
+    "link": "http://ipla-e1-81.pluscdn.pl/p/movies/80/804323ee9ae2fb283039e40453f3cc1c.mp4"
+  },
+  {
+    "id": 13,
+    "title": "S01E13 - Kiepscy dają czadu",
+    "link": "http://ipla-e1-81.pluscdn.pl/p/movies/24/249c8e21ebc21963e959a9c97c78eb24.mp4"
+  },
+  {
+    "id": 14,
+    "title": "S01E14 - Właściciele",
+    "link": "http://ipla-e2-22.pluscdn.pl/p/movies/9d/9d312398bb0cd5ccbf3f8c33bbd811fe.mp4"
+  },
+  {
+    "id": 15,
+    "title": "S01E15 - Wal magistra",
+    "link": "http://ipla-e1-76.pluscdn.pl/p/movies/df/df70a97488cc8e81969461667900f755.mp4"
+  }
+];
+
+function App() {
+
+  return (
+    <h1>test</h1>
+  )
+}
+
+export default App
